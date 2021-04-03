b46326b8df6b6dff2016fd3497e323857fdd9087
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-03 17:01:08 +0000

    getting post routes for waitData to work

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const reservations = [];
-const waitlist = [];
+const waitData = [];
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -18,13 +18,13 @@ app.get("/reserve", (req, res) => {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.post("/reserve", (req, res) => {
+app.post("/api/tables", (req, res) => {
   const newReservation = req.body;
 
   console.log(newReservation);
 
-  reservations.push(newReservation);
-  console.log(reservations);
+  waitData.push(newReservation);
+  console.log(waitData);
   res.json(newReservation);
 });
 
@@ -32,7 +32,15 @@ app.get("/tables", (req, res) => {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.post("/tables", (req, res) => {});
+app.get("/api/tables", (req, res) => {
+  res.json(reservations);
+});
+
+app.get("/api/waitlist", (req, res) => {
+  res.json(waitData);
+});
+
+// app.post("/tables", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
