4eaf0b7d46fdc02f71979d1c1e5e1259a8431564
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-03 15:14:37 +0000

    added get and post routes

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", (req, res) => {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.post("/reserve", (req, res) => {});
+
+app.get("/tables", (req, res) => {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.post("/tables", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
