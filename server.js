d2ce2f6ea590442363fc58d46116c8a68b2708dd
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-03 15:26:44 +0000

    working on reserve post

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const reservations = [];
+const waitlist = [];
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -15,7 +18,16 @@ app.get("/reserve", (req, res) => {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.post("/reserve", (req, res) => {});
+app.post("/reserve", (req, res) => {
+  const newReservation = req.body;
+
+  newReservation.name = newReservation.name.replace(/\s+/g, "");
+  console.log(newReservation);
+
+  reservations.push(newReservation);
+  console.log(reservations);
+  res.json(newReservation);
+});
 
 app.get("/tables", (req, res) => {
   res.sendFile(path.join(__dirname, "tables.html"));
